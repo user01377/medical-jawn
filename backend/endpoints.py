0c1b01e656c36b5f291b0b4a2b18d51010876c1a
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2026-02-28 14:55:48 -0500

    get diastolic json

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 
 def get_db_row(user_id, table, db_path):
@@ -40,14 +41,69 @@ def get_bloodpressure(user_id):
     finally:
         con.close()
 
+# returns json object for Systolic blood pressure
+def get_systolic(user_id):
+    con = sqlite3.connect('blood.sqlite')
+    cur = con.cursor()
+
+    systolic = {}
+
+    try:
+        query = '''
+        SELECT date, systolic
+        FROM blood_pressure
+        WHERE user_id = ?
+        ORDER BY date
+        '''
+        cur.execute(query, (user_id,))
+        rows = cur.fetchall()
+
+        for date, sys_val in rows:
+            systolic[date] = sys_val
+
+        return  json.dumps(systolic)
+
+    except sqlite3.Error as e:
+        print("Failed to get blood pressure:", e)
+        return None
+
+    finally:
+        con.close()
+
+# returns json object for Diastolic blood pressure
+def get_diastolic(user_id):
+    con = sqlite3.connect('blood.sqlite')
+    cur = con.cursor()
+
+    systolic = {}
+
+    try:
+        query = '''
+        SELECT date, diastolic
+        FROM blood_pressure
+        WHERE user_id = ?
+        ORDER BY date
+        '''
+        cur.execute(query, (user_id,))
+        rows = cur.fetchall()
+
+        for date, sys_val in rows:
+            systolic[date] = sys_val
+
+        return  json.dumps(systolic)
+
+    except sqlite3.Error as e:
+        print("Failed to get blood pressure:", e)
+        return None
+
+    finally:
+        con.close()
 
 def main():
-    row = get_db_row(1, 'blood_pressure' ,'blood.sqlite')
-
-    if row:
-        print(row)
-    else:
-        print("No blood pressure record found.")
+    bp_s = get_systolic(1)
+    bp_d = get_diastolic(1)
+    print(bp_s)
+    print(bp_d)
 
 
 if __name__ == '__main__':
