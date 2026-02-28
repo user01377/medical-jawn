d59d1d4bb0279c4e137e1887e97d4f3bcc5f8a37
Dev Bolton <jabolton939@gmail.com>
2026-02-28 18:44:14 -0500

    Dev/main (#8)
    * dev main push
    
    * dev main push
    
    * added venv and endpoint file
    
    * Dev/backendpoints (#2)
    
    * ahhhhh
    
    * ahhhhh
    
    * created prototype get_bloodpressure
    
    * db-creation (#1)
    
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    * added date field
    
    * get diastolic json
    
    ---------
    
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    * Dev/backendpoints (#5)
    
    * ahhhhh
    
    * ahhhhh
    
    * created prototype get_bloodpressure
    
    * db-creation (#1)
    
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    * added date field
    
    * get diastolic json
    
    * added get avg functions
    
    * bug fix time
    
    * fixedtheinsertthing
    
    * added get all patients
    
    * deleted sqlite files
    
    ---------
    
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    * added sqlite database files into git ignore
    
    * Dev/backendpoints (#7)
    
    * ahhhhh
    
    * ahhhhh
    
    * created prototype get_bloodpressure
    
    * db-creation (#1)
    
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    * added date field
    
    * get diastolic json
    
    * added get avg functions
    
    * bug fix time
    
    * fixedtheinsertthing
    
    * added get all patients
    
    * deleted sqlite files
    
    * fixed systolic endpoint
    
    * vincent ahh
    
    ---------
    
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
    Co-authored-by: Clouu0 <clouvou0@gmail.com>
    
    ---------
    
    Co-authored-by: Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
    Co-authored-by: Vincent Vue <vuevincent2025@gmail.com>
    Co-authored-by: Clouu0 <clouvou0@gmail.com>

---
+++ backend/db.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+conn = sqlite3.connect('users.sqlite')
+cursor = conn.cursor()
+
+# Create patient table in main database
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS patient (
+        user_id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER NOT NULL,
+        weight INTEGER NOT NULL,
+        height INTEGER NOT NULL
+    )
+''')
+
+conn.commit()
+
+# Attach second database
+cursor.execute("ATTACH DATABASE 'blood.sqlite' AS blood")
+
+# Create blood_pressure table in attached database
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS blood.blood_pressure (
+        user_id INTEGER,
+        systolic INTEGER NOT NULL,
+        diastolic INTEGER NOT NULL,
+        "date" TEXT NOT NULL
+    )
+''')
+
+
+conn.commit()
---
+++ backend/endpoints.py
@@ -0,0 +1,134 @@
+import sqlite3
+import json
+import numpy as np
+
+# returns tuple of a full row in database
+def get_db_row(user_id, table, db_path):
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    try:
+        query = f"SELECT * FROM {table} WHERE user_id = ?"
+        cur.execute(query, (user_id,))
+        row = cur.fetchone()
+        return row
+
+    except sqlite3.Error as e:
+        print("Invalid DB query or connection failed:", e)
+        return None
+
+    finally:
+        con.close()
+
+# returns user IDs and names
+def get_all_patients():
+    con = sqlite3.connect('users.sqlite')
+    cur = con.cursor()
+
+    cur.execute('SELECT user_id, name FROM patient')
+    print(cur.fetchall())
+
+    con.close()
+
+
+# returns all bloodpressure data of patient
+def get_bloodpressure(user_id):
+    con = sqlite3.connect('blood.sqlite')
+    cur = con.cursor()
+
+    try:
+        query = '''
+        SELECT systolic, diastolic, date
+        FROM blood_pressure
+        WHERE user_id = ?
+        '''
+        cur.execute(query, (user_id,))
+        row = cur.fetchall()
+        return row
+
+    except sqlite3.Error as e:
+        print("Failed to get blood pressure:", e)
+        return None
+
+    finally:
+        con.close()
+
+# returns json object for Systolic blood pressure
+def get_systolic(user_id):
+    con = sqlite3.connect('blood.sqlite')
+    cur = con.cursor()
+
+    systolic = dict()
+
+    try:
+        query = '''
+        SELECT date, systolic
+        FROM blood_pressure
+        WHERE user_id = ?
+        ORDER BY date
+        '''
+        cur.execute(query, (user_id,))
+        rows = cur.fetchall()
+
+        for date, sys_val in rows:
+            systolic[date] = sys_val
+
+        return json.dumps(systolic)
+
+    except sqlite3.Error as e:
+        print("Failed to get blood pressure:", e)
+        return None
+
+    finally:
+        con.close()
+
+# returns json object for Diastolic blood pressure
+def get_diastolic(user_id):
+    con = sqlite3.connect('blood.sqlite')
+    cur = con.cursor()
+
+    systolic = dict()
+
+    try:
+        query = '''
+        SELECT date, diastolic
+        FROM blood_pressure
+        WHERE user_id = ?
+        ORDER BY date
+        '''
+        cur.execute(query, (user_id,))
+        rows = cur.fetchall()
+
+        for date, sys_val in rows:
+            systolic[date] = sys_val
+
+        return  json.dumps(systolic)
+
+    except sqlite3.Error as e:
+        print("Failed to get blood pressure:", e)
+        return None
+
+    finally:
+        con.close()
+
+# returns the average of a patients systolic levels
+def get_systolic_avg(user_id):
+    sys = json.loads(get_systolic(user_id))
+
+    vals = np.array(sorted(dict.values(sys)))
+    return np.mean(vals)
+
+# returns the average of a patients diastolic levels
+def get_diastolic_avg(user_id):
+    dia = json.loads(get_diastolic(user_id))
+
+    vals = np.array(sorted(dict.values(dia)))
+    return np.mean(vals)
+
+def main():
+    print(get_diastolic(2))
+    print(get_diastolic_avg(2))
+
+
+if __name__ == '__main__':
+    main()
