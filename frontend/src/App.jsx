c2567914259ab64c78bda6bc1148e84416ec8600
Vincent Vue <vuevincent2025@gmail.com>
2026-02-28 17:35:45 -0500

    used react router make the links on homepage active

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import './styles/App.css'
-import IndexPage from './pages/index-page';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/index-page";
+import PatientsPage from "./pages/patients-page";
 
 function App() {
   return <IndexPage />;
