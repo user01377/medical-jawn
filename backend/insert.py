59e0b7b558b05fa931512c691896b9f8f39ce39c
Clouu0 <clouvou0@gmail.com>
2026-02-28 17:18:38 -0500

    fixedtheinsertthing

--- backend/insert.py
+++ backend/insert.py
@@ -1,10 +1,10 @@
 import sqlite3
 import numpy as np
-def add_patient(user_id, name, age, weight, height):
+def add_patient(name, age, weight, height):
     conn = sqlite3.connect('users.sqlite')
     cursor = conn.cursor()
 
-    cursor.execute(f"INSERT INTO patient (user_id, name, age, weight, height) VALUES (?, ?, ?, ?)", (user_id, name, age, weight, height))
+    cursor.execute(f"INSERT INTO patient (name, age, weight, height) VALUES (?, ?, ?, ?)", (name, age, weight, height))
     conn.commit()
 
 def add_blood_pressure(user_id, systolic, diastolic, date):
@@ -19,21 +19,17 @@ def add_blood_pressure(user_id, systolic, diastolic, date):
 
     conn.commit()
     conn.close()
-
-def update_or_add_patient(user_id, name, age, weight, height, systolic, diastolic, date):
+    
+def update_patient(user_id, systolic, diastolic, date):
     conn = sqlite3.connect('users.sqlite')
-    cursor = conn.cursor()
-
-    add_patient(user_id, name, age, weight, height)
 
     try:
         #cursor.execute('SELECT user_id FROM patient WHERE user_id = ?', (user_id,))
 
-        add_patient(name, age, weight, height)
 
         add_blood_pressure(user_id, systolic, diastolic, date)
-    except:
-        print('except')
+    except Exception as e:
+        print(f'except: {e}')
 
 
     finally:
@@ -45,18 +41,30 @@ def main():
     sys = rng.integers(90, 361, 20)
     dia = rng.integers(50, 361, 20)
     year = 1987
-
+    add_patient(name='Jamk Performative', age=35, weight=180, height=70)
     for i in range(len(sys)):
-        update_or_add_patient(
+        update_patient(
             user_id=1,
-            name="John Wic",
-            age=21+i,
-            weight=167,
-            height=185,
             systolic=int(sys[i]),
             diastolic=int(dia[i]),
             date='2020-3-21'
         )
-
+    add_patient(name='Vindent', age=67, weight=10, height=80)
+    for i in range(len(sys)):
+        update_patient(
+            user_id=2,
+            systolic=int(sys[i]),
+            diastolic=int(dia[i]),
+            date=f'{year+i}-3-21'
+        )
+    add_patient(name='Sigma Ryan', age=5000, weight=1830, height=6767)
+    for i in range(len(sys)):
+        update_patient(
+            user_id=3,
+            systolic=int(sys[i]),
+            diastolic=int(dia[i]),
+            date=f'{year-i}-3-21'
+        )
+        
 if __name__ == "__main__":
     main()
