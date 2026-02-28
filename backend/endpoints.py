488b7351b968d6ef44101f6fc0068710d9f9c899
Jacob Bolton <jacobbolton@Jacobs-MacBook-Pro-2.local>
2026-02-28 13:28:12 -0500

    created prototype get_bloodpressure

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sqlite3
 
+
+# Returns tuple containing data for specific row in a db table
 def get_db_row(id, subtable, db_path):
     con = sqlite3.connect(db_path)
     cur = con.cursor()
@@ -10,6 +12,22 @@ def get_db_row(id, subtable, db_path):
         return query
     except:
         print('invalid db query or connection failed.')
+        return []
+    finally:
+        if con:
+            con.close()
+
+def get_bloodpressure(id, subtable):
+    con = sqlite3.connect('blood.blood_pressure')
+    cur = con.cursor()
+
+    try:
+        dia = cur.execute('SELECT blood.blood_pressure.diastolic')
+        sys = cur.execute('SELECT blood.blood_pressure.systolic')
+        print(dia)
+        print(sys)
+    except:
+        print('failed to get bloodpressure')
         return []
     finally:
         if con:
