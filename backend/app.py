2072f3c0f4295d73c592eed952fcf06c5a3b1f0b
Vincent Vue <vuevincent2025@gmail.com>
2026-02-28 19:41:58 -0500

    defined basic route for frontend to fetch all patients

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import endpoints
 import sqlite3
 import json
 
 app = FastAPI()
 
-# this is the file to expose endpoints to our frontend
-
+@app.get("/all-patients")
+def read_all_patients():
+    return endpoints.get_all_patients()
